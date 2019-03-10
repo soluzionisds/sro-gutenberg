e327834db826563fea929d6e236d91f34630d123
Sebastiano Riva <sebastiano.riva@gmail.com>
2019-03-10 18:10:02 +0100

    setup

---
+++ block-section/block-section.js
@@ -0,0 +1,53 @@
+// **********************************************************************
+// Category Options: common, formatting, layout, widgets, embed
+//
+
+wp.blocks.registerBlockType('sro-gutenberg/block-section', {
+  title: 'Block Section',
+  icon: 'grid-view',
+  category: 'common',
+  attributes: {
+    title: { type: 'string', },
+    color: { type: 'string', },
+    link: { type: 'string', },
+  },
+
+  edit: function(props) {
+    function updateTitle(event) {
+      props.setAttributes({title: event.target.value})
+    }
+    function updateLink(event) {
+      props.setAttributes({link: event.target.value})
+    }
+    function updateColor(event) {
+      props.setAttributes({color: event.target.value})
+    }
+
+    return wp.element.createElement(
+      "div", { className: 'sro-gutenberg-block-section-backend-style' },
+      wp.element.createElement("h4", { className: 'sro-gutenberg-block-section-backend-style__title'}, "Section Block"),
+      wp.element.createElement("input", { type: "text", placeholder: "Title", className: 'sro-gutenberg-block-section-backend-style__input', value: props.attributes.title, onChange: updateTitle }),
+      wp.element.createElement("input", { type: "text", placeholder: "Link", className: 'sro-gutenberg-block-section-backend-style__input', value: props.attributes.link, onChange: updateLink }),
+      wp.element.createElement("input", { type: "text", placeholder: "Color (hexadecimal)", className: 'sro-gutenberg-block-section-backend-style__input', value: props.attributes.color, onChange: updateColor }),
+    );
+  },
+
+  save: function(props) {
+
+    return wp.element.createElement(
+      "div", { style: { 'background-color': props.attributes.color } },
+      wp.element.createElement("a", { href: props.attributes.link, className: 'wp-block-sro-gutenberg-block-section__link' },
+        wp.element.createElement("span", { className: 'wp-block-sro-gutenberg-block-section__title' }, props.attributes.title )
+      )
+    );
+  },
+})
+
+
+
+/*return wp.element.createElement(
+  "div", { style: { background-color: props.attributes.color } },
+  wp.element.createElement( "a", { className: title },
+    return wp.element.createElement( "span", props.attributes.title ),
+  ),
+);*/
---
+++ sro-gutenberg.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Plugin Name: sr(o) Gutenberg Blocks
+Plugin URI:  https://github.com/soluzionisds/eurscva.git
+Description: The custom Gutenberg blocks of sebastiano.riva (office)
+Author: sebastiano.riva (office)
+Author URI: http://www.sebastianoriva.com/
+Text Domain: sro-gutenberg
+Version: 0.1
+*/
+
+defined('ABSPATH') || exit;
+
+if ( ! function_exists( 'sro_gutenberg' ) ) {
+  function sro_gutenberg() {
+    wp_register_script(
+      'sro-gutenberg-block-section-backend-script',
+      plugins_url( 'block-section/block-section.js', __FILE__ ),
+      array( 'wp-blocks', 'wp-i18n', 'wp-element' )
+    );
+    wp_register_style(
+      'sro-gutenberg-block-section-backend-style',
+      plugins_url( 'block-section/editor.css', __FILE__ ),
+      array( 'wp-edit-blocks' ),
+      filemtime( plugin_dir_path( __FILE__ ) . 'block-section/editor.css' )
+    );
+    wp_register_style(
+       'sro-gutenberg-block-section-frontend-style',
+       plugins_url( 'block-section/style.css', __FILE__ ),
+       array(),
+       filemtime( plugin_dir_path( __FILE__ ) . 'block-section/style.css' )
+   );
+    register_block_type( 'sro-gutenberg/block-section', array(
+      'editor_script' => 'sro-gutenberg-block-section-backend-script',
+      'editor_style'  => 'sro-gutenberg-block-section-backend-style',
+      'style' => 'sro-gutenberg-block-section-frontend-style',
+    ));
+  }
+  add_action( 'init', 'sro_gutenberg' );
+}
+
+/*if ( ! function_exists( 'sro_gutenberg_frontend' ) ) {
+  function sro_gutenberg_frontend() {
+  	wp_enqueue_style(
+  		'sro-gutenberg-block-section-frontend-style',
+  		plugins_url( 'block-section/style.css', __FILE__ ),
+  		array( 'wp-blocks' ),
+  		filemtime( plugin_dir_path( __FILE__ ) . 'block-section/style.css' )
+  	);
+  }
+  add_action( 'enqueue_block_assets', 'sro_gutenberg_frontend' );
+}*/
+
+/*function loadMyBlockFiles() {
+  wp_enqueue_script(
+    'my-super-unique-handle',
+    plugin_dir_url(__FILE__) . 'my-block.js',
+    array('wp-blocks', 'wp-i18n', 'wp-editor'),
+    true
+  );
+}
+
+add_action('enqueue_block_editor_assets', 'loadMyBlockFiles');*/
